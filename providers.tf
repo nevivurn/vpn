c1231392041bc2c78cc432e862c8f438a645a4db
Yongun Seong <nevivurn@riseup.net>
2020-08-11 21:12:57 +0900

    Update to terraform 0.13

--- providers.tf
+++ providers.tf
@@ -1,12 +1,6 @@
 provider "google" {
-  version = "~> 3.33.0"
-
   credentials = var.credentials
   project     = var.project
   region      = var.region
   zone        = var.zone
-}
-
-provider "local" {
-  version = "~> 1.4.0"
 }
---
+++ versions.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_version = ">= 0.13"
+
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 3.33.0"
+    }
+    local = {
+      source  = "hashicorp/local"
+      version = "~> 1.4.0"
+    }
+  }
+}
